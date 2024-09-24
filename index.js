41af9198117c3e45ccff2b4b2225c2756b9d370e
Shivam <shivamagarwalmarch@gmail.com>
2024-09-24 14:31:59 +1000

    Updating

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ const results = document.querySelector('.results')
 const searchInput = document.querySelector('#searchInput')
 
 // Firebase is now available globally
-const { initializeApp, getDatabase, ref, set, get, remove } = window.firebase;
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "movie-watchlist-18757.firebaseapp.com",
-  projectId: "movie-watchlist-18757",
-  storageBucket: "movie-watchlist-18757.appspot.com",
-  messagingSenderId: "805387743764",
-  appId: "1:805387743764:web:1d88b6c60f2d5885742d01",
-  measurementId: "G-6975S4HSXS",
-  databaseURL: "https://movie-watchlist-18757-default-rtdb.asia-southeast1.firebasedatabase.app"
-};
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, get, remove } from 'firebase/database';
+import { firebaseConfig } from './firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
